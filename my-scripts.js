d567b1481aec17b329edee98bcf846ca8047ac4e
tkynaston <42258599+tkynaston@users.noreply.github.com>
2018-08-11 09:56:01 -0600

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,8 @@
+
+function turnOn() {
+	document.getElementById('light-bulb').src="https://image.flaticon.com/icons/svg/167/167745.svg";
+}
+
+function turnOff() {
+	document.getElementById('light-bulb').src="https://image.flaticon.com/icons/svg/158/158272.svg";
+}
